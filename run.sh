c4f683a7989ab04193ef2860757c2c23a7f69d7b
Marcelo Souza <marcelo@marcelosouza.com>
2018-08-13 14:37:23 -0300

    Changing run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /opt/OpenDXL-Anomali-STAXX/staxx_exporter.py -c /opt/OpenDXL-Anomali-STAXX/conf/client.conf -l $LOG_LEVEL -p -t $POLLING_TIME $SEARCH_STRING
+python /opt/OpenDXL-Anomali-STAXX/staxx_exporter.py -c /opt/OpenDXL-Anomali-STAXX/conf/client.conf -l $LOG_LEVEL -p -t $POLLING_TIME "$FILTER_QUERY"
